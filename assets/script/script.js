4e548e233f0adff25f2fb0ac9c9b96b9964a80f1
Kyle Fohrman <kylegfohrman@gmail.com>
2022-07-17 21:47:42 -0400

    hamburger menu functionality on small screen devices

--- assets/script/script.js
+++ assets/script/script.js
@@ -8,3 +8,20 @@ scrollUp.addEventListener("click", () => {
         behavior: "smooth",
     });
 });
+
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+    ul.classList.toggle("show");
+  });
+
+  const navLink = document.querySelectorAll(".nav-link");
+
+  navLink.forEach((link) =>
+    link.addEventListener("click", () => {
+      ul.classList.remove("show");
+    })
+  );
+
